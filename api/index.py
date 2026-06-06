16a7e2964657f2ebc892139bd4982eee5cdfdb27
Ariba Sayed <aribaa@Aribas-MacBook-Air.local>
2026-06-06 13:01:25 +0530

    Initial commit

---
+++ api/index.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import json
+import numpy as np
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["POST"],
+    allow_headers=["*"]
+)
+
+with open("telemetry.json") as f:
+    telemetry = json.load(f)
+
+class RequestBody(BaseModel):
+    regions: list[str]
+    threshold_ms: int
+
+@app.post("/")
+def metrics(data: RequestBody):
+
+    result = {}
+
+    for region in data.regions:
+
+        rows = [
+            r for r in telemetry
+            if r["region"] == region
+        ]
+
+        latencies = [
+            r["latency_ms"]
+            for r in rows
+        ]
+
+        uptimes = [
+            r["uptime"]
+            for r in rows
+        ]
+
+        result[region] = {
+            "avg_latency": sum(latencies)/len(latencies),
+            "p95_latency": float(
+                np.percentile(latencies,95)
+            ),
+            "avg_uptime": sum(uptimes)/len(uptimes),
+            "breaches": sum(
+                1 for x in latencies
+                if x > data.threshold_ms
+            )
+        }
+
+    return result
