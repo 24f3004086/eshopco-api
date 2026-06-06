92a0f3d332ea841a155d6cb9d4d1c996b962dd6d
24f3004086 <aribasayed2005@gmai.com>
2026-06-06 13:57:10 +0530

    changes 3

--- api/index.py
+++ api/index.py
@@ -7,10 +7,11 @@ import numpy as np
 app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["POST"],
-    allow_headers=["*"]
+CORSMiddleware,
+allow_origins=["*"],
+allow_methods=["POST", "GET", "OPTIONS"],
+allow_headers=["Content-Type", "Authorization"],
+expose_headers=["Access-Control-Allow-Origin"],
 )
 
 from pathlib import Path
